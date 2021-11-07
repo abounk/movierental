97d11cdb597fac0d20e852239aed5f64608f5681
abounk <a_bunk911@hotmail.com>
2021-11-07 23:07:44 +0700

    Add attributes to Movie

--- movie.py
+++ movie.py
@@ -29,17 +29,32 @@ class Movie:
     A movie available for rent.
     """
 
-    def __init__(self, title, price_code: PriceCode):
+    def __init__(self, title, year, genre):
         # Initialize a new movie.
-        self.title = title
-        self.price_code = price_code
+        self.__title = title
+        self.__price_code = 0
+        self.__year = year
+        self.__genre = genre
 
     def get_price_code(self):
         # get the price code
-        return self.price_code
+        return self.__price_code
+
+    def set_price_code(self, value):
+        self.__price_code = value
 
     def get_title(self):
-        return self.title
+        return self.__title
+
+    def get_year(self):
+        return self.__year
+
+    def get_genre(self):
+        return self.__genre
+
+    def is_genre(self, genre):
+        return True if genre in self.__genre \
+            else False
 
     def __str__(self):
-        return self.title
+        return self.__title
