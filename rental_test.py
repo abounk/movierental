20f561af3d385d7704dbe0a4ebf99da991e9211c
abounk <a_bunk911@hotmail.com>
2021-12-05 18:23:53 +0700

    Update test cases to use subtest

--- rental_test.py
+++ rental_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import unittest
 from customer import Customer
 from rental import Rental
@@ -8,36 +9,39 @@ from movie_catalog import MovieCatalog
 class RentalTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_movie = Movie("Mulan", 2021, ['Action', 'Adventure'])
-        self.regular_movie = Movie("CitizenFour", 2013, ['Documentary'])
+        self.current_year = datetime.now().year
+        self.new_movie = Movie("Mulan", self.current_year, [
+                               'Action', 'Adventure'])
+        self.regular_movie = Movie(
+            "CitizenFour", self.current_year - 1, ['Documentary'])
         self.childrens_movie = Movie(
-            "Frozen", 2020, ['Action', 'Adventure', 'Children'])
+            "Frozen", self.current_year - 1, ['Action', 'Adventure', 'Children'])
 
     def test_movie_attributes(self):
         """trivial test to catch refactoring errors or change in API of Movie"""
-        m = Movie("CitizenFour", 2013, ['Documentary'])
+        m = Movie("CitizenFour", self.current_year - 1, ['Documentary'])
         self.assertEqual("CitizenFour", m.get_title())
 
     def test_rental_price(self):
-        rental = Rental(self.new_movie, 1, PriceCode.new_release)
-        self.assertEqual(rental.get_charge(), 3.0)
-        rental = Rental(self.new_movie, 5, PriceCode.new_release)
-        self.assertEqual(rental.get_charge(), 15.0)
-        rental = Rental(self.regular_movie, 3, PriceCode.regular)
-        self.assertEqual(rental.get_charge(), 1.5)
-        rental = Rental(self.regular_movie, 1, PriceCode.regular)
-        self.assertEqual(rental.get_charge(), 2.0)
-        rental = Rental(self.childrens_movie, 4, PriceCode.childrens)
-        self.assertEqual(rental.get_charge(), 1.5)
-        rental = Rental(self.childrens_movie, 2, PriceCode.childrens)
-        self.assertEqual(rental.get_charge(), 1.5)
+        test_cases = [
+            (Rental(self.new_movie, 1), 3.0),
+            (Rental(self.new_movie, 5), 15.0),
+            (Rental(self.regular_movie, 3), 1.5),
+            (Rental(self.regular_movie, 1), 2.0),
+            (Rental(self.childrens_movie, 4), 1.5),
+            (Rental(self.childrens_movie, 2), 1.5)
+        ]
+        for test in test_cases:
+            with self.subTest():
+                self.assertEqual(test[0].get_charge(), test[1])
 
     def test_rental_points(self):
-        rental = Rental(self.new_movie, 1, PriceCode.new_release)
-        self.assertEqual(rental.get_rental_points(), 1)
-        rental = Rental(self.new_movie, 5, PriceCode.new_release)
-        self.assertEqual(rental.get_rental_points(), 5)
-        rental = Rental(self.regular_movie, 3, PriceCode.regular)
-        self.assertEqual(rental.get_rental_points(), 1)
-        rental = Rental(self.childrens_movie, 4, PriceCode.childrens)
-        self.assertEqual(rental.get_rental_points(), 1)
+        test_cases = [
+            (Rental(self.new_movie, 1), 1),
+            (Rental(self.new_movie, 5), 5),
+            (Rental(self.regular_movie, 3), 1),
+            (Rental(self.childrens_movie, 4), 1)
+        ]
+        for test in test_cases:
+            with self.subTest():
+                self.assertEqual(test[0].get_rental_points(), test[1])
